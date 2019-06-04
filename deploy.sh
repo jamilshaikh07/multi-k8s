93d3451b0e2931bba7df4f3e073a997a11e208bd
James <james@Jamess-MacBook-Pro.local>
2019-06-04 19:03:48 +0530

    StephenGrider docker build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t jamilshaikh07/multi-client:latest -t jamilshaikh07/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t jamilshaikh07/multi-server:latest -t jamilshaikh07/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t jamilshaikh07/multi-worker:latest -t jamilshaikh07/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push jamilshaikh07/multi-client:latest
 docker push jamilshaikh07/multi-server:latest
 docker push jamilshaikh07/multi-worker:latest
@@ -10,6 +11,7 @@ docker push jamilshaikh07/multi-server:$SHA
 docker push jamilshaikh07/multi-worker:$SHA
 
 kubectl apply -f k8s
+
 kubectl set image deployments/server-deployment server=jamilshaikh07/multi-server:$SHA
 kubectl set image deployments/server-deployment client=jamilshaikh07/multi-client:$SHA
 kubectl set image deployments/server-deployment worker=jamilshaikh07/multi-worker:$SHA
